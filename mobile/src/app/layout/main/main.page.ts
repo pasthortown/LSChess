bd434b459dcac83a61c1387b34cb0917d58189d4
pasthortown <pastorchessmaster@gmail.com>
2019-09-26 15:11:03 -0500

    hola

--- mobile/src/app/layout/main/main.page.ts
+++ mobile/src/app/layout/main/main.page.ts
@@ -34,26 +34,12 @@ export class MainPage implements OnInit {
   user = '';
   current_move = 0;
   checking_move = 0;
-  currentScreenOrientation = '';
   sizeLeftPanel = 8;
   sizeRightPanel = 4;
 
   constructor(private pickerCtrl: PickerController, private screenOrientation: ScreenOrientation) { 
     this.board = new Board(this.chess.fen().split(' ')[0],5);
-    this.screenOrientation.onChange().subscribe(
-      () => {
-          this.currentScreenOrientation = this.screenOrientation.type;
-          if (this.currentScreenOrientation.toUpperCase() == this.screenOrientation.ORIENTATIONS.LANDSCAPE_PRIMARY
-          || this.currentScreenOrientation.toUpperCase() == this.screenOrientation.ORIENTATIONS.LANDSCAPE_SECONDARY
-          || this.currentScreenOrientation.toUpperCase() == this.screenOrientation.ORIENTATIONS.LANDSCAPE) {
-            this.sizeLeftPanel = 8;
-            this.sizeRightPanel = 4;
-          } else {
-            this.sizeLeftPanel = 12;
-            this.sizeRightPanel = 12;
-          }
-      }
-    );
+    this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.LANDSCAPE);
   }
 
   ngOnInit() {
