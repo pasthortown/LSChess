4898e87b3288d991eb8ffd16c2bf3579ebf26a03
pasthortown <pastorchessmaster@gmail.com>
2019-09-26 14:32:09 -0500

    hola

--- mobile/src/app/layout/main/main.module.ts
+++ mobile/src/app/layout/main/main.module.ts
@@ -6,6 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { MainPage } from './main.page';
+import { ScreenOrientation } from '@ionic-native/screen-orientation/ngx';
 
 const routes: Routes = [
   {
@@ -20,6 +21,9 @@ const routes: Routes = [
     FormsModule,
     IonicModule,
     RouterModule.forChild(routes)
+  ],
+  providers: [
+    ScreenOrientation,
   ],
   declarations: [MainPage]
 })
